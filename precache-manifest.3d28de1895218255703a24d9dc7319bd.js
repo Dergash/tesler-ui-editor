0c39ac8f5dc23fecd9ba4ab41e3347f7911eb0d1
Dergash <Dergash@users.noreply.github.com>
2020-06-26 10:48:01 +0000

    deploy: 6be49ecf30b8d5e0114150cf98ad747bfebeec4f

---
+++ precache-manifest.3d28de1895218255703a24d9dc7319bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819212c8263427822c962746293e1eb3",
+    "url": "/tesler-ui-editor/index.html"
+  },
+  {
+    "revision": "17ef3533bcc0dccdeb29",
+    "url": "/tesler-ui-editor/static/css/2.2e872fcd.chunk.css"
+  },
+  {
+    "revision": "b28d8a6059b7cbadcd78",
+    "url": "/tesler-ui-editor/static/css/main.b8e79692.chunk.css"
+  },
+  {
+    "revision": "17ef3533bcc0dccdeb29",
+    "url": "/tesler-ui-editor/static/js/2.0257078e.chunk.js"
+  },
+  {
+    "revision": "a5c92df6f0cd985d39e596d920f6bf71",
+    "url": "/tesler-ui-editor/static/js/2.0257078e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28d8a6059b7cbadcd78",
+    "url": "/tesler-ui-editor/static/js/main.3b3c4bff.chunk.js"
+  },
+  {
+    "revision": "58c0bb8736dcf3633de0",
+    "url": "/tesler-ui-editor/static/js/runtime-main.a353061b.js"
+  }
+]);
